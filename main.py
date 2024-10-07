c906bada49c5dda7cea0b794e83676096db7703e
Kent Jasper Cabunoc Sisi <65537922+kntjspr@users.noreply.github.com>
2024-10-07 10:01:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -47,60 +47,60 @@ def cloudflare():
     input()
     element[0].click()
 
-
-chrome_options = Options()
-
-# Custom chromdriver, browser and user data path
-chrome_options.binary_location = os.path.dirname(os.path.realpath(__file__)) + "\\chrome\\chrome.exe" # Change to browser binary path
-chrome_service = Service('chromedriver.exe')
-# Set chrome user data on the project folder. It should be named 'data_dir'.
-chrome_options.add_argument('--user-data-dir=' + os.path.dirname(os.path.realpath(__file__)) + '\\data_dir')
-
-# Add proxy capabilities
-use_proxy = False
-if(use_proxy == True):
-    # Change to appropriate values.
-    proxy = 'proxy-server.com:1080' 
-    chrome_options.add_argument('--proxy-server=socks5://' + proxy)
-    print("Proxy is enabled.")  
-
-
-
-# Bypass Selenium Detection
-chrome_options.add_experimental_option("excludeSwitches", ['enable-automation', 'enable-logging'])
-# Remove "profile.managed_default_content_settings.images" if you wish to enable images. Disabled on default for data saving.
-prefs = {
-        "credentials_enable_service": False,
-        "profile.password_manager_enabled": False,
-        'disk-cache-size': 4096,
-        "profile.managed_default_content_settings.images": 2
-    }
-chrome_options.add_experimental_option("prefs", prefs)
-
-chrome_options.add_argument('--disable-blink-features=AutomationControlled')
-chrome_options.add_argument("--remote-debugging-port=9223")
-chrome_options.add_argument("--disable-3d-apis")
-chrome_options.add_argument("--disable-notifications")
-chrome_options.add_argument("--disable-crash-reporter")
-chrome_options.add_argument("--no-sandbox");
-chrome_options.add_argument("--disable-dev-shm-usage");
-chrome_options.add_argument("--disable-renderer-backgrounding");
-chrome_options.add_argument("--disable-background-timer-throttling");
-chrome_options.add_argument("--disable-backgrounding-occluded-windows");
-chrome_options.add_argument("--disable-client-side-phishing-detection");
-chrome_options.add_argument("--disable-crash-reporter");
-chrome_options.add_argument("--disable-oopr-debug-crash-dump");
-chrome_options.add_argument("--no-crash-upload");
-chrome_options.add_argument("--disable-gpu");
-#chrome_options.add_argument("--disable-extensions");
-chrome_options.add_argument("--disable-low-res-tiling");
-chrome_options.add_argument("--log-level=3");
-
-
-# Initiate selenium driver
-driver = webdriver.Chrome(options=chrome_options)
-clear_tabs()
-
-navigate("https://google.com")
-time.sleep(5)
-print("Done")
+if __name__ == "__main__":
+    chrome_options = Options()
+    
+    # Custom chromedriver, browser and user data path
+    chrome_options.binary_location = os.path.dirname(os.path.realpath(__file__)) + "\\chrome\\chrome.exe" # Change to browser binary path
+    chrome_service = Service('chromedriver.exe')
+    # Set chrome user data on the project folder. It should be named 'data_dir'.
+    chrome_options.add_argument('--user-data-dir=' + os.path.dirname(os.path.realpath(__file__)) + '\\data_dir')
+    
+    # Add proxy capabilities
+    use_proxy = False
+    if(use_proxy == True):
+        # Change to appropriate values.
+        proxy = 'proxy-server.com:1080' 
+        chrome_options.add_argument('--proxy-server=socks5://' + proxy)
+        print("Proxy is enabled.")  
+    
+    
+    
+    # Bypass Selenium Detection
+    chrome_options.add_experimental_option("excludeSwitches", ['enable-automation', 'enable-logging'])
+    # Remove "profile.managed_default_content_settings.images" if you wish to enable images. Disabled on default to save bandwidth.
+    prefs = {
+            "credentials_enable_service": False,
+            "profile.password_manager_enabled": False,
+            'disk-cache-size': 4096,
+            "profile.managed_default_content_settings.images": 2
+        }
+    chrome_options.add_experimental_option("prefs", prefs)
+    
+    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+    chrome_options.add_argument("--remote-debugging-port=9223")
+    chrome_options.add_argument("--disable-3d-apis")
+    chrome_options.add_argument("--disable-notifications")
+    chrome_options.add_argument("--disable-crash-reporter")
+    chrome_options.add_argument("--no-sandbox");
+    chrome_options.add_argument("--disable-dev-shm-usage");
+    chrome_options.add_argument("--disable-renderer-backgrounding");
+    chrome_options.add_argument("--disable-background-timer-throttling");
+    chrome_options.add_argument("--disable-backgrounding-occluded-windows");
+    chrome_options.add_argument("--disable-client-side-phishing-detection");
+    chrome_options.add_argument("--disable-crash-reporter");
+    chrome_options.add_argument("--disable-oopr-debug-crash-dump");
+    chrome_options.add_argument("--no-crash-upload");
+    chrome_options.add_argument("--disable-gpu");
+    #chrome_options.add_argument("--disable-extensions");
+    chrome_options.add_argument("--disable-low-res-tiling");
+    chrome_options.add_argument("--log-level=3");
+    
+    
+    # Initiate selenium driver
+    driver = webdriver.Chrome(options=chrome_options)
+    clear_tabs()
+    
+    navigate("https://google.com")
+    time.sleep(5)
+    print("Done")
